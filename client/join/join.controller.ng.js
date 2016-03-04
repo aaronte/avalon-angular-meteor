eda59f9b0e05198b1073b1fc572b267fffdbb34c
Chuan <c_p_827@Hotmail.com>
2016-03-04 13:54:12 -0500

    tried fixing html to submit on enter, didnt work. fixed join controller issue, use fetch() after db.find() calls, and then index the returned array accordingly

--- client/join/join.controller.ng.js
+++ client/join/join.controller.ng.js
@@ -15,23 +15,28 @@ function JoinController($reactive, $scope, $state, userService) {
     vm.joinRoom = joinRoom;
 
     function joinRoom() {
-        var existingRoom = Rooms.findOne({code:  vm.getReactively('form.roomCode')});
+        var existingRoom = Rooms.find({code:  vm.getReactively('form.roomCode')}).fetch();
+
 
         if (existingRoom) {
             console.log('Joining ', vm.form.roomCode, ' as ', vm.form.userName);
 
             var currentUser = {
                 userName: vm.form.userName,
-                roomId: existingRoom._id,
+                roomId: existingRoom[0]._id,
                 master: false
             };
             currentUser._id = Users.insert(currentUser);
 
             userService.setModel(currentUser);
-            $state.go('/room');
+            Session.set('userId', currentUser._id);
+            $state.go('room');
 
             vm.form = {roomCode: '', userName: ''};
             return;
+        }
+        else {
+            console.log('Room ', vm.form.roomCode, ' does not exist.');
         }
 
         console.log('Cant find room.');
