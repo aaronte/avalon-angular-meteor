dde1b48587858cc23a5154a64a2a8c889ec8282f
Chuan <c_p_827@Hotmail.com>
2016-03-04 14:03:21 -0500

    added helper functions: getCode to retrieve game room code from roomId, nextMaster if master leaves room,  isGameStarted.  added regular functions for gameStarting to update gameStarted field when Start is pressed, isMaster to check user's master field

--- client/room/room.controller.ng.js
+++ client/room/room.controller.ng.js
@@ -4,32 +4,66 @@ angular
     .module('avalonMeteorApp')
     .controller('RoomController', RoomController);
 
-RoomController.$inject = ['$reactive', '$scope', 'userService'];
+RoomController.$inject = ['$reactive', '$scope', 'userService', '$state'];
 
-function RoomController($reactive, $scope, userService) {
+function RoomController($reactive, $scope, userService, $state) {
     $reactive(this).attach($scope);
     var vm = this;
 
-    vm.user = userService.getModel();
 
-    vm.code = code;
+    vm.user = userService.getModel();
     vm.leaveRoom = leaveRoom;
+    vm.gameStart = false;
+    vm.isMaster = isMaster;
+
+
+
+
+    vm.gameStarting = gameStarting;
+
 
     vm.helpers({
         participants: function () {
-            return Users.find({roomId: vm.user.roomId});
+            return Users.find({roomId: vm.user.roomId}).fetch();
+
         },
         hasEnoughPlayers: function () {
-            var numberOfRequiredPlayers = 5;
-            return numberOfRequiredPlayers <= Users.find({roomId: vm.user.roomId}).length;
+            var numberOfRequiredPlayers = 3;
+            return numberOfRequiredPlayers <= Users.find({roomId: vm.user.roomId}).fetch().length;
+        },
+        getCode: function () {
+            return Rooms.find({_id: vm.user.roomId}).fetch()[0];
+        },
+        nextMaster: function () {
+            var participantsInRoom = Users.find({roomId: vm.user.roomId}).fetch();
+            var userId = Session.get('userId');
+            if (userId == participantsInRoom[0]._id){
+                vm.user.master = true;
+                Users.update({_id:Session.get('userId')},{$set:{'master':true}});
+            }
+        },
+        isGameStarted: function () {
+            var inGame = Rooms.find({_id:vm.user.roomId}).fetch();
+            if(inGame[0].gameStarted){
+                $state.go('game');
+            }
+            return inGame[0].gameStarted;
         }
+
     });
 
-    function code() {
-        return vm.user.roomId;
+
+    function isMaster() {
+        return vm.getReactively('user.master');
+
     }
 
     function leaveRoom() {
         Users.remove({_id: vm.user._id});
     }
+
+    function gameStarting() {
+        Rooms.update({_id:vm.user.roomId},{$set:{'gameStarted':true}});
+    }
+
 }
