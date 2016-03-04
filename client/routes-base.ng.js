5193d34c567afb27a9e136605a8e6bef7535a1b5
Chuan <c_p_827@Hotmail.com>
2016-03-04 13:56:01 -0500

    added state routing from main-router-ng.js

--- client/routes-base.ng.js
+++ client/routes-base.ng.js
@@ -1,18 +1,44 @@
-'use strict';
+angular.module('avalonMeteorApp').config(function ($urlRouterProvider, $stateProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
 
-angular
-    .module('avalonMeteorApp')
-    .config(function ($urlRouterProvider, $locationProvider) {
-        $locationProvider.html5Mode(true);
-        $urlRouterProvider.otherwise('/');
-    }).run(['$rootScope', '$state', function ($rootScope, $state) {
-    $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
-        switch (error) {
-            case 'AUTH_REQUIRED':
-            case 'FORBIDDEN':
-            case 'UNAUTHORIZED':
+    $stateProvider
+        .state('main', {
+            url: '/',
+            templateUrl: 'client/main/main.view.ng.html'
+
+        })
+        .state('create', {
+            url: '/create',
+            templateUrl: 'client/create/create.view.ng.html',
+            controller: 'CreateController',
+            controllerAs: 'createVm'
+
+        })
+        .state('join', {
+            url: '/join',
+            templateUrl: 'client/join/join.view.ng.html',
+            controller: 'JoinController',
+            controllerAs: 'joinVm'
+        })
+        .state('room', {
+            url: '/room',
+            templateUrl: 'client/room/room.view.ng.html',
+            controller: 'RoomController',
+            controllerAs: 'roomVm'
+        })
+        .state('game', {
+            url: '/game',
+            templateUrl: 'client/game/game.view.ng.html',
+            controller: 'GameController',
+            controllerAs: 'gameVm'
+        });
+
+    $urlRouterProvider.otherwise("/");
+})
+    .run(function ($rootScope, $state) {
+        $rootScope.$on('stateChangeError', function (event, toState, toParams, fromState, fromParams, error){
+            if (error === 'AUTH_REQUIRED') {
                 $state.go('main');
-                break;
-        }
+            }
+        });
     });
-}]);
