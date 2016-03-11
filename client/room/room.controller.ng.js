4af12a4cb20d610ac4f66d685ffbf352e26382b4
Chuan <c_p_827@Hotmail.com>
2016-03-11 00:23:23 -0500

    Added conditional special roles, restriction logic for valid games

--- client/room/room.controller.ng.js
+++ client/room/room.controller.ng.js
@@ -19,10 +19,10 @@ function RoomController($reactive, $scope, userService, $state, $mdDialog) {
     vm.participantIsMaster = participantIsMaster;
 
 
+
     vm.helpers({
         participants: function () {
             return Users.find({roomId: vm.user.roomId}).fetch();
-
         },
         hasEnoughPlayers: function () {
             var numberOfRequiredPlayers = 2;
@@ -47,14 +47,16 @@ function RoomController($reactive, $scope, userService, $state, $mdDialog) {
         }
     });
 
-
     function isMaster() {
         return vm.getReactively('user.master');
-
     }
 
     function participantIsMaster(participantId) {
-        return Users.find({_id: participantId}).fetch()[0].master;
+        try {
+            return Users.find({_id: participantId}).fetch()[0].master;
+        }
+        catch(err){
+        }
     }
 
     function leaveRoom() {
@@ -66,6 +68,7 @@ function RoomController($reactive, $scope, userService, $state, $mdDialog) {
     }
 
     vm.savedRoles = {};
+    vm.numParticipants = vm.participants.length;
     vm.showSettingsDialog = function (ev) {
         $mdDialog.show({
             controller: DialogController,
@@ -75,22 +78,58 @@ function RoomController($reactive, $scope, userService, $state, $mdDialog) {
             targetEvent: ev,
             clickOutsideToClose: true,
             locals: {
-                savedRoles : vm.savedRoles
+                savedRoles : vm.savedRoles,
+                numParticipants: vm.numParticipants
             }
         })
             .then(function(savedData) {
                 vm.savedRoles = savedData;
         });
     };
-    function DialogController($mdDialog, savedRoles) {
+    function DialogController($mdDialog, savedRoles, numParticipants) {
         var vm = this;
         vm.selectedRoles = angular.copy(savedRoles);
+        vm.selectedBadRoles = 0x0;
+
+        vm.checkMerlin = function() {
+            vm.selectedRoles.Assassin = !vm.selectedRoles.Assassin;
+            if(vm.selectedRoles.Percival) vm.selectedRoles.Percival = !vm.selectedRoles.Percival;
+            if(vm.selectedRoles.Mordred) vm.selectedRoles.Mordred = !vm.selectedRoles.Mordred;
+        };
+
+        vm.checkPercival = function() {
+            if(vm.selectedRoles.Morgana) vm.selectedRoles.Morgana = !vm.selectedRoles.Morgana;
+        };
+
+        vm.checkBadRoles = function() {
+            var selectedAssassin = 0x0;
+            var selectedMorgana = 0x0;
+            var selectedMordred = 0x0;
+            var selectedOberon = 0x0;
+
+            if (vm.selectedRoles.Assassin) selectedAssassin = 0x8;
+            if (vm.selectedRoles.Morgana) selectedMorgana = 0x4;
+            if (vm.selectedRoles.Mordred) selectedMordred = 0x2;
+            if (vm.selectedRoles.Oberon) selectedOberon = 0x1;
+
+            vm.selectedBadRoles = (selectedAssassin | selectedMorgana | selectedMordred | selectedOberon);
+        };
 
         vm.closeDialog = function() {
             $mdDialog.cancel();
         };
         vm.save = function(savedData) {
             $mdDialog.hide(savedData);
+        };
+
+        vm.disableOberon = function() {
+            return (numParticipants < 7) ? ((vm.selectedBadRoles == 0xA) || (vm.selectedBadRoles == 0xC)) : (numParticipants < 10) ? vm.selectedBadRoles == 0xE : false;
+        };
+        vm.disableMordred = function() {
+            return (numParticipants < 7) ? ((vm.selectedBadRoles == 0xC) || (vm.selectedBadRoles == 0x9)): (numParticipants < 10) ? vm.selectedBadRoles == 0xD : false;
+        };
+        vm.disableMorgana = function() {
+            return (numParticipants < 7) ? ((vm.selectedBadRoles == 0xA) || (vm.selectedBadRoles == 0x9)): (numParticipants < 10) ? vm.selectedBadRoles == 0xB : false;
         };
     }
 
