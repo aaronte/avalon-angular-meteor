aa6667baac81a3feac7718c4913a80ca5869938e
Chuan <c_p_827@Hotmail.com>
2016-03-23 00:17:31 -0400

    Added display for special role text

--- client/game/game.controller.ng.js
+++ client/game/game.controller.ng.js
@@ -10,6 +10,10 @@ function GameController ($reactive, $scope, userService, $state) {
 
     vm.user = userService.getModel();
     vm.endGame = endGame;
+    vm.isMerlin = isMerlin;
+    vm.isPercival = isPercival;
+    vm.isEvil = isEvil;
+
 
     vm.helpers({
         isGameEnded: function () {
@@ -20,14 +24,56 @@ function GameController ($reactive, $scope, userService, $state) {
         },
         getRole: function () {
             vm.user.role = Users.find({_id: vm.user._id}).fetch()[0].role;
-            console.log(vm.user.role);
             return vm.user.role;
+        },
+        getEvilUserNames: function () {
+            vm.selectedRoles = Rooms.find({_id: vm.user.roomId}).fetch()[0].selectedRoles;
+            var badPlayers = [];
+            var minionsOfMordredPlayers = Users.find({roomId: vm.user.roomId, role: 'Minions of Mordred'}).fetch();
+            for (var i = 0; i < minionsOfMordredPlayers.length; i ++) {
+                badPlayers.push(minionsOfMordredPlayers[i].userName);
+            }
+            if (vm.selectedRoles.Assassin){
+                badPlayers.push(Users.find({roomId: vm.user.roomId, role: 'Assassin'}).fetch()[0].userName);
+            }
+            if (vm.selectedRoles.Morgana){
+                badPlayers.push(Users.find({roomId: vm.user.roomId, role: 'Morgana'}).fetch()[0].userName);
+            }
+            if (vm.user.role != 'Assassin' && vm.user.role != 'Morgana' && vm.user.role != 'Mordred' && vm.user.role != 'Minions of Mordred') {
+                if (vm.selectedRoles.Oberon){
+                    badPlayers.push(Users.find({roomId: vm.user.roomId, role: 'Oberon'}).fetch()[0].userName);
+                }
+            }
+            if(vm.user.role != 'Merlin') {
+                if (vm.selectedRoles.Mordred){
+                    badPlayers.push(Users.find({roomId: vm.user.roomId, role: 'Mordred'}).fetch()[0].userName);
+                }
+            }
+            return _.shuffle(badPlayers);
+        },
+        getMerlinUserNames: function () {
+            vm.selectedRoles = Rooms.find({_id: vm.user.roomId}).fetch()[0].selectedRoles;
+            var merlinPlayers = [];
+            if (vm.selectedRoles.Merlin){
+                merlinPlayers.push(Users.find({roomId: vm.user.roomId, role: 'Merlin'}).fetch()[0].userName);
+            }
+            if (vm.selectedRoles.Morgana){
+                merlinPlayers.push(Users.find({roomId: vm.user.roomId, role: 'Morgana'}).fetch()[0].userName);
+            }
+            return _.shuffle(merlinPlayers);
         }
     });
 
     function endGame(){
         Rooms.update({_id: vm.user.roomId},{$set: {'gameStarted': false}});
     }
-
-
+    function isMerlin () {
+        return vm.user.role === 'Merlin';
+    }
+    function isPercival () {
+        return vm.user.role === 'Percival';
+    }
+    function isEvil () {
+        return vm.user.role === 'Minions of Mordred' || vm.user.role === 'Assassin' || vm.user.role === 'Mordred' || vm.user.role === 'Morgana';
+    }
 }
